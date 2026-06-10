63248f5fc4a4defbc43b155bf5c3d65f8aefbaed
Péter Király <pkiraly@gwdg.de>
2026-06-10 19:58:34 +0200

    Overview page: pdf download cuts content #12

--- classes/IssuesDBMySQL.php
+++ classes/IssuesDBMySQL.php
@@ -138,7 +138,7 @@ class IssuesDBMySQL {
   public function getFrequency($schema = '', $provider_id = '', $set_id = '', $file = '') {
     $this->values = [];
     $where = $this->getWhere($schema, $provider_id, $set_id, '');
-    $stmt = $this->db->prepare('SELECT field, value, frequency FROM frequency ' . $where . ' ORDER BY field');
+    $stmt = $this->db->prepare('SELECT field, value, frequency FROM frequency ' . $where . ' ORDER BY field, value');
     $this->bindValues($schema, $provider_id, $set_id, '', $stmt);
 
     $stmt->execute();
